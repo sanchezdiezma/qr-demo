70a986a6bb1d370989b1232a95360bca2ce072d5
Jesus M Sanchez Diezma <91361497+sanchezdiezma@users.noreply.github.com>
2024-05-31 11:37:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function redirectToStore() {
 
     // Detecta si el dispositivo es Android
     if (/android/i.test(userAgent)) {
-        window.location.href = "https://play.google.com/store/apps/details?id=tu.app.android";
+        window.location.href = "https://play.google.com/store/apps/details?id=com.hoteligy.princess&gl=ES";
     }
     // Detecta si el dispositivo es iOS
     else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        window.location.href = "https://apps.apple.com/us/app/tu-app-ios/idXXXXXXXX";
+        window.location.href = "https://apps.apple.com/es/app/princess-hotels-resorts/id6446177684";
     } else {
         // Si no es Android ni iOS, muestra un mensaje o redirige a una página web
         document.getElementById("message").innerText = "Por favor, visita esta página desde un dispositivo móvil para descargar la app.";
